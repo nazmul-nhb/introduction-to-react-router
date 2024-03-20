4c89d92dc4cc7c7187feeba99be61db6af8044fd
nazmul-nhb <nazmulnhb007@yahoo.com>
2024-03-20 10:15:08 +0600

    installed tailwind css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,8 @@
 const Header = () => {
     return (
         <div>
-            <h2>Navigation</h2>
-            <nav>
+            <h2 className="text-green-800">Navigation</h2>
+            <nav className="flex justify-between gap-4">
                 <a href="/Home">Home</a>
                 <a href="/about">About</a>
                 <a href="/contact">Contact Us</a>
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import Home from './components/Home/Home.jsx';
-import Header from './components/Header/Header.jsx';
 import About from './components/About/About.jsx';
 import Contact from './components/Contact/Contact.jsx';
 
