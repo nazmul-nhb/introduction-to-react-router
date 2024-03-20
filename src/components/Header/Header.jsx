b6f7c3669df3d2430024336792a31726a3b015a7
nazmul-nhb <nazmulnhb007@yahoo.com>
2024-03-20 16:16:19 +0600

    used active route, useNavigation etc.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,15 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
     return (
         <div className="max-w-[1920px] w-full md:px-24 mt-16 mx-auto flex flex-col gap-8">
             <h2 className="text-green-800 font-extrabold text-4xl">Introduction to React Router</h2>
             <nav className="flex justify-between gap-5">
-                <Link to={'/'}>Home</Link>
-                <Link to={'/users'}>Users</Link>
-                <Link to={'/posts'}>Posts</Link>
-                <Link to={'/about'}>About</Link>
-                <Link to={'/contact'}>Contact Us</Link>
+                <NavLink to={'/'}>Home</NavLink>
+                <NavLink to={'/users'}>Users</NavLink>
+                <NavLink to={'/posts'}>Posts</NavLink>
+                <NavLink to={'/about'}>About</NavLink>
+                <NavLink to={'/contact'}>Contact Us</NavLink>
             </nav>
         </div>
     );
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,21 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigation } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 
 const Home = () => {
+
+    const navigation = useNavigation();
+    const location = useLocation();
+    console.log(location);
+
     return (
         <div className="">
             <Header></Header>
-            <Outlet></Outlet>
+            {
+                navigation.state === "loading" ?
+                    <p className="text-center my-8">Loading...</p> :
+                    <Outlet></Outlet>
+            }
             <Footer></Footer>
         </div>
     );
